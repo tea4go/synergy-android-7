96c2eb2760308d036cfbb7d4742f883795ac6189
Ammar Githam <ammar.githam@outlook.com>
2022-02-06 01:34:39 +0900

    Convert EventQueueTimer to kotlin

--- app/src/main/java/org/synergy/base/EventQueueTimer.kt
+++ app/src/main/java/org/synergy/base/EventQueueTimer.kt
@@ -17,44 +17,27 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.synergy.base;
+package org.synergy.base
 
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*
 
-public class EventQueueTimer {
+class EventQueueTimer(
+    timeout: Double, // Do it once?
+    var oneShot: Boolean, // The target for the event
+    var target: Any, // What will run when the timer is fired from the Event Queue
+    var job: EventJobInterface
+) {
+    private val timer = Timer()
 
-    // Do it once?
-    boolean oneShot;
-
-    // The target for the event
-    Object target;
-
-    // What will run when the timer is fired from the Event Queue
-    EventJobInterface job;
-
-    Timer timer;
-
-    public EventQueueTimer(double timeout, boolean oneShot, Object target, EventJobInterface job) {
-        if (job == null || target == null) {
-            throw new IllegalArgumentException("Target and job must not be null");
-        }
-
-        this.oneShot = oneShot;
-        this.target = target;
-        this.job = job;
-
-        this.timer = new Timer();
-        timer.schedule(new TimerEventTask(), (long) (timeout * 1000.0));
+    init {
+        timer.schedule(TimerEventTask(), (timeout * 1000.0).toLong())
     }
 
     /**
      * Cancel a running timer
      */
-    public void cancel() {
-        if (timer != null) {
-            timer.cancel();
-        }
+    fun cancel() {
+        timer.cancel()
     }
 
     /**
@@ -62,91 +45,20 @@ public class EventQueueTimer {
      * This task will take the EventJob and create a new
      * timer event and put it onto the EventQueue.  The Event Queue will
      * actually handle the dispatching of the job
-     * <p>
+     *
+     *
      * Hmm. Scratch that?  Just run the damn thing?
      */
-    private class TimerEventTask extends TimerTask {
-        public void run() {
-            job.run(new Event(EventType.TIMER, target));
-			/*Log.debug ("Timer fired");
-			
-			//EventQueue.getInstance ().adoptHandler (EventType.TIMER, target, job);
-			EventQueue.getInstance ().addEvent (new Event (EventType.TIMER, target)); //, null, Event.Flags.DELIVER_IMMEDIATELY));
-			*/
+    private inner class TimerEventTask : TimerTask() {
+        override fun run() {
+            job.run(Event(EventType.TIMER, target))
 
+            // Log.debug ("Timer fired");
+            // EventQueue.getInstance ().adoptHandler (EventType.TIMER, target, job);
+            // EventQueue.getInstance ().addEvent (new Event (EventType.TIMER, target)); //, null, Event.Flags.DELIVER_IMMEDIATELY));
             if (!oneShot) {
-                timer.cancel();
+                timer.cancel()
             }
         }
     }
-	
-	
- /*   private EventQueueTimer timer;
-    private double timeout;
-    private double time;
-    private Object target;
-    private boolean oneShot;
-
-    public EventQueueTimer (Timer timer, double timeout, double initialTime,
-            Object target, boolean oneShot) {
-
-        this.timer = timer;
-        this.timeout = timeout;
-        this.target = target;
-        this.oneShot = oneShot;
-        this.time = initialTime;
-
-        // TODO assert (timeout > 0.0)
-    }
-
-    public void reset () {
-        time = timeout;
-    }
-
-    public boolean isOneShot () {
-        return oneShot;
-    }
-
-    public void subtractTime (double dt) {
-        time -= dt;
-    }
-
-    public double getTime () {
-        return time;
-    }
-
-
-    public EventQueueTimer getTimer () {
-        return timer;
-    }
-
-    public Object getTarget () {
-        return target;
-    }
-
-    public void fillEvent (TimerEvent event) {
-        event.setTimer (timer);
-        event.setCount (0);
-
-        if (time <= 0.0) {
-            event.setCount (new Integer ((int)((timeout - time) / timeout)));
-        }
-    }
-    
-    public int compareTo (Object o) {
-        /*if (!o.instanceof (Timer)) {
-            return -1;
-        }
-        *
-        EventQueueTimer rhs = (EventQueueTimer) o;
-
-        if (time < rhs.getTime ()) {
-            return -1;
-        } else if (time == rhs.getTime ()) {
-            return 0;
-        } else {
-            return 1;
-        }
-    } */
-
 }
