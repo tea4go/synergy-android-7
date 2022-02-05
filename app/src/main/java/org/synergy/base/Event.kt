1568660f483e2420cfcb0f4c83b1f1dd4a621917
Ammar Githam <ammar.githam@outlook.com>
2022-02-05 19:23:15 +0900

    Convert Event to kotlin

--- app/src/main/java/org/synergy/base/Event.kt
+++ app/src/main/java/org/synergy/base/Event.kt
@@ -17,88 +17,35 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package org.synergy.base
 
-package org.synergy.base;
-
-import org.jetbrains.annotations.NotNull;
+import kotlin.jvm.JvmOverloads
 
 /**
  * @author Shaun Patterson
  */
-public class Event {
-
-    public enum Flags {
+class Event @JvmOverloads constructor(
+    val type: EventType = EventType.UNKNOWN,
+    val target: Any? = null,
+    val data: Any? = null,
+    val flags: Flags = Flags.NONE,
+) {
+    enum class Flags(val value: Int) {
         NONE(0x00),
         DELIVER_IMMEDIATELY(0x01),
-        DONTFREEDATA(0x02);
+        DONTFREEDATA(0x02),
+    }
 
-        private int value;
+    val name = ""
 
-        Flags(int value) {
-            this.value = value;
+    override fun toString(): String {
+        return "Event:$type:$target"
+    }
+
+    companion object {
+        @JvmStatic
+        fun deleteData(event: Event?) {
+            // TODO
         }
-
-        public int getValue() {
-            return value;
-        }
-
-    }
-
-    private EventType type;
-    private Object target;
-    private Object data;
-    private Event.Flags flags;
-    private String name = "";
-
-    public Event() {
-        this(EventType.UNKNOWN, null, null, Flags.NONE);
-    }
-
-    public Event(EventType type) {
-        this(type, null, null, Flags.NONE);
-    }
-
-    public Event(EventType type, Object target) {
-        this(type, target, null, Flags.NONE);
-    }
-
-    public Event(EventType type, Object target, Object data) {
-        this(type, target, data, Flags.NONE);
-    }
-
-    public Event(EventType type, Object target, Object data, Flags flags) {
-        this.type = type;
-        this.target = target;
-        this.data = data;
-        this.flags = flags;
-    }
-
-    public static void deleteData(final Event event) {
-        // TODO
-    }
-
-    public EventType getType() {
-        return type;
-    }
-
-    public Object getTarget() {
-        return target;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public Event.Flags getFlags() {
-        return flags;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    @NotNull
-    public String toString() {
-        return "Event:" + type.toString() + ":" + target;
     }
 }
