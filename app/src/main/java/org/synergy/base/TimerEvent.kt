8a8e6d202b435ee6a24cfa0b4676c51c4ba3af64
Ammar Githam <ammar.githam@outlook.com>
2022-02-05 19:31:49 +0900

    Convert TimerEvent to kotlin

--- app/src/main/java/org/synergy/base/TimerEvent.kt
+++ app/src/main/java/org/synergy/base/TimerEvent.kt
@@ -17,34 +17,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.synergy.base;
+package org.synergy.base
 
-public class TimerEvent {
-
-    // The timer
-    private EventQueueTimer timer;
-
-    // Number of repeats
-    private Integer count;
-
-    public TimerEvent(EventQueueTimer timer, Integer count) {
-        this.timer = timer;
-        this.count = count;
-    }
-
-    public EventQueueTimer getTimer() {
-        return timer;
-    }
-
-    public void setTimer(EventQueueTimer timer) {
-        this.timer = timer;
-    }
-
-    public Integer getCount() {
-        return count;
-    }
-
-    public void setCount(Integer count) {
-        this.count = count;
-    }
-}
+data class TimerEvent(
+    var timer: EventQueueTimer, // The timer
+    var count: Int, // Number of repeats
+)
