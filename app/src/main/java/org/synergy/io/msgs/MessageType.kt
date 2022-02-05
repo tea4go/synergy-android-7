67ba55b3f771556ce90670c16366cca5aadcde1a
Ammar Githam <ammar.githam@outlook.com>
2022-02-06 00:40:49 +0900

    Convert MessageType to kotlin

--- app/src/main/java/org/synergy/io/msgs/MessageType.kt
+++ app/src/main/java/org/synergy/io/msgs/MessageType.kt
@@ -17,63 +17,128 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.synergy.io.msgs;
+package org.synergy.io.msgs
 
 /**
- * For more information on the Synergy message format, see 
+ * For more information on the Synergy message format, see
  * http://synergy-foss.org/code/filedetails.php?repname=synergy&path=%2Ftrunk%2Fsrc%2Flib%2Fsynergy%2FProtocolTypes.cpp
  */
-public enum MessageType {
-    HELLO ("Barrier", "[Init] Hello"),           // Not a standard message
-    HELLOBACK ("Barrier", "[Init] Hello Back"),  // Not a standard message
-    CNOOP ("CNOP", "[Command] NoOp"),
-    CCLOSE ("CBYE", "[Command] Close"),
-    CENTER ("CINN", "[Command] Enter"),
-    CLEAVE ("COUT", "[Command] Leave"),
-    CCLIPBOARD ("CCLP", "[Command] Clipboard"),
-    CSCREENSAVER ("CSEC", "[Command] Screensaver"),
-    CRESETOPTIONS ("CROP", "[Command] Reset Options"),
-    CINFOACK ("CIAK", "[Command] Info Ack"),
-    CKEEPALIVE ("CALV", "[Command] Keep Alive"),
-    DKEYDOWN ("DKDN", "[Data] Key Down"),
-    DKEYREPEAT ("DKRP", "[Data] Key Repeat"),
-    DKEYUP ("DKUP", "[Data] Key Up"),
-    DMOUSEDOWN ("DMDN", "[Data] Mouse Down"),
-    DMOUSEUP ("DMUP", "[Data] Mouse Up"),
-    DMOUSEMOVE ("DMMV", "[Data] Mouse Move"),
-    DMOUSERELMOVE ("DMRM", "[Data] Mouse Relative Move"),
-    DMOUSEWHEEL ("DMWM", "[Data] Mouse Wheel"),
-    DCLIPBOARD ("DCLP", "[Data] Clipboard"),
-    DINFO ("DINF", "[Data] Info"),
-    DSETOPTIONS ("DSOP", "[Data] Set Options"),
-    QINFO ("QINF", "[Query] Info"),
-    EINCOMPATIBLE ("EICV", "[Error] Incompatible"),
-    EBUSY ("EBSY", "[Error] Busy"),
-    EUNKNOWN ("EUNK", "[Error] Unknown"),
-    EBAD ("EBAD", "[Error] Bad");
+enum class MessageType(val value: String, private val commonName: String) {
+    HELLO(
+        "Barrier",
+        "[Init] Hello"
+    ),  // Not a standard message
+    HELLOBACK(
+        "Barrier",
+        "[Init] Hello Back"
+    ),  // Not a standard message
+    CNOOP(
+        "CNOP",
+        "[Command] NoOp"
+    ),
+    CCLOSE(
+        "CBYE",
+        "[Command] Close"
+    ),
+    CENTER(
+        "CINN",
+        "[Command] Enter"
+    ),
+    CLEAVE(
+        "COUT",
+        "[Command] Leave"
+    ),
+    CCLIPBOARD(
+        "CCLP",
+        "[Command] Clipboard"
+    ),
+    CSCREENSAVER(
+        "CSEC",
+        "[Command] Screensaver"
+    ),
+    CRESETOPTIONS(
+        "CROP",
+        "[Command] Reset Options"
+    ),
+    CINFOACK(
+        "CIAK",
+        "[Command] Info Ack"
+    ),
+    CKEEPALIVE(
+        "CALV",
+        "[Command] Keep Alive"
+    ),
+    DKEYDOWN(
+        "DKDN",
+        "[Data] Key Down"
+    ),
+    DKEYREPEAT(
+        "DKRP",
+        "[Data] Key Repeat"
+    ),
+    DKEYUP(
+        "DKUP",
+        "[Data] Key Up"
+    ),
+    DMOUSEDOWN(
+        "DMDN",
+        "[Data] Mouse Down"
+    ),
+    DMOUSEUP(
+        "DMUP",
+        "[Data] Mouse Up"
+    ),
+    DMOUSEMOVE(
+        "DMMV",
+        "[Data] Mouse Move"
+    ),
+    DMOUSERELMOVE(
+        "DMRM",
+        "[Data] Mouse Relative Move"
+    ),
+    DMOUSEWHEEL(
+        "DMWM",
+        "[Data] Mouse Wheel"
+    ),
+    DCLIPBOARD(
+        "DCLP",
+        "[Data] Clipboard"
+    ),
+    DINFO(
+        "DINF",
+        "[Data] Info"
+    ),
+    DSETOPTIONS(
+        "DSOP",
+        "[Data] Set Options"
+    ),
+    QINFO(
+        "QINF",
+        "[Query] Info"
+    ),
+    EINCOMPATIBLE(
+        "EICV",
+        "[Error] Incompatible"
+    ),
+    EBUSY(
+        "EBSY",
+        "[Error] Busy"
+    ),
+    EUNKNOWN(
+        "EUNK",
+        "[Error] Unknown"
+    ),
+    EBAD(
+        "EBAD",
+        "[Error] Bad"
+    );
 
-    private MessageType (String value, String commonName) {
-        this.value = value;
-        this.commonName = commonName;
+    override fun toString() = commonName
+
+    companion object {
+        @JvmStatic
+        fun fromString(messageValue: String) = values().find {
+            messageValue.equals(it.value, ignoreCase = true)
+        } ?: throw IllegalArgumentException("No MessageType with value $messageValue")
     }
-
-    public static MessageType fromString (String messageValue) {
-        for (MessageType t : MessageType.values ()) {
-            if (messageValue.equalsIgnoreCase (t.value)) {
-                return t;
-            }
-        }
-        throw new IllegalArgumentException ("No MessageType with value " + messageValue);
-    }
-
-    public String getValue () {
-        return value;
-    }
-
-    public String toString () {
-        return commonName;
-    }
-
-    private String value;
-    private String commonName;
 }
