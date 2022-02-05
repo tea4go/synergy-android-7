4ed0476f9f74d476b8caa4fc8e4535e5cabc950a
Ammar Githam <ammar.githam@outlook.com>
2022-02-05 19:58:47 +0900

    Convert NetworkAddress to kotlin

--- app/src/main/java/org/synergy/net/NetworkAddress.kt
+++ app/src/main/java/org/synergy/net/NetworkAddress.kt
@@ -17,72 +17,28 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.synergy.net;
+package org.synergy.net
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
+import java.net.InetAddress
+import java.net.UnknownHostException
 
 /**
  * @author Shaun Patterson
  */
-public class NetworkAddress {
+class NetworkAddress(hostname: String, port: Int) {
+    var address: InetAddress? = null
+        private set
+    var hostname: String = hostname
+        private set
+    var port = port
+        private set
 
-    private InetAddress address;
-    private String hostname = "";
-    private int port = 0;
-
-
-    public NetworkAddress() {
-    }
-
-    public NetworkAddress(int port) {
-    }
-
-    public NetworkAddress(final String hostname, final int port) {
-        this.hostname = hostname;
-        this.port = port;
-    }
-
-    protected void finalize() throws Throwable {
-        super.finalize();
-
-        // TODO close address
-    }
-
-    public void resolve() throws UnknownHostException {
+    @Throws(UnknownHostException::class)
+    fun resolve() {
         if (address != null) {
             // Discard previous address
-
-            address = null;
+            address = null
         }
-
-        if (hostname == null) {
-            System.out.println("Hostname is empty");
-        } else {
-            address = InetAddress.getByName(hostname);
-        }
-    }
-
-    public boolean isValid() {
-        return true;
-    }
-
-    public InetAddress getAddress() {
-        return address;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public String getHostname() {
-        return hostname;
-    }
-
-    private void checkPort() {
-    }
-
-    boolean equalTo(NetworkAddress address) {
-        return false;
+        address = InetAddress.getByName(hostname)
     }
 }
