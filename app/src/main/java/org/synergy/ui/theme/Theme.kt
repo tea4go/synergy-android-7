56fbe7be96bc7c2b955b9744eeaec54feae1db82
Ammar Githam <ammar.githam@outlook.com>
2022-02-10 02:09:38 +0900

    Add BarrierClientTheme for compose

---
+++ app/src/main/java/org/synergy/ui/theme/Theme.kt
@@ -0,0 +1,30 @@
+package org.synergy.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors()
+
+private val LightColorPalette = lightColors()
+
+@Composable
+fun BarrierClientTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        shapes = Shapes(),
+        content = content
+    )
+}
