0364b2ce90b92e52337f3ef7a236a8df61e1eb5d
Ammar Githam <ammar.githam@outlook.com>
2022-02-12 17:59:11 +0900

    Add scaffold to MainContent

--- app/src/main/java/org/synergy/MainContent.kt
+++ app/src/main/java/org/synergy/MainContent.kt
@@ -1,16 +1,61 @@
 package org.synergy
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import org.synergy.ui.screens.home.HomeScreen
 
 @Composable
-fun MainContent() {
+fun MainContent(
+    modifier: Modifier = Modifier,
+) {
     val navController = rememberNavController()
+    val scaffoldState = rememberScaffoldState()
 
-    NavHost(navController = navController, startDestination = "home") {
+    Scaffold(
+        modifier = modifier,
+        scaffoldState = scaffoldState,
+        topBar = { MainTopBar() },
+        content = {
+            MainNavHost(
+                modifier = Modifier.padding(it),
+                navController = navController,
+            )
+        }
+    )
+}
+
+@Composable
+private fun MainTopBar(
+    modifier: Modifier = Modifier,
+) {
+    TopAppBar(
+        modifier = modifier,
+        title = {
+            Text(text = stringResource(id = R.string.app_name))
+        },
+    )
+}
+
+@Composable
+private fun MainNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = "home",
+    ) {
         composable("home") {
             HomeScreen()
         }
