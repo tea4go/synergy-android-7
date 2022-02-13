c2013df84204dd3022ee2649e5d3d9c37677dd5d
Ammar Githam <ammar.githam@outlook.com>
2022-02-13 16:20:53 +0900

    Move HomeScreenContent to its own file

--- app/src/main/java/org/synergy/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/org/synergy/ui/screens/home/HomeScreen.kt
@@ -4,7 +4,6 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
-import android.content.res.Configuration
 import android.net.Uri
 import android.os.Build
 import android.os.IBinder
@@ -15,29 +14,18 @@ import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import org.synergy.R
 import org.synergy.barrier.base.utils.Timber
 import org.synergy.barrier.base.utils.e
-import org.synergy.data.db.entities.ServerConfig
 import org.synergy.services.BarrierClientService
-import org.synergy.ui.common.FixPermissionsBanner
 import org.synergy.ui.common.OnLifecycleEvent
-import org.synergy.ui.common.ServerConfigForm
-import org.synergy.ui.theme.BarrierClientTheme
 import org.synergy.utils.DisplayUtils
 
 @Composable
@@ -172,127 +160,6 @@ fun HomeScreen(
             viewModel.setShowAccessibilityPermissionDialog(false)
         }
     )
-}
-
-@Composable
-private fun HomeScreenContent(
-    modifier: Modifier = Modifier,
-    serverConfig: ServerConfig = ServerConfig(),
-    barrierClientConnected: Boolean = false,
-    hasOverlayDrawPermission: Boolean = false,
-    hasAccessibilityPermission: Boolean = false,
-    showOverlayDrawPermissionDialog: Boolean = false,
-    showAccessibilityPermissionDialog: Boolean = false,
-    onServerConfigChange: (ServerConfig) -> Unit = {},
-    onConnectClick: () -> Unit = {},
-    onFixPermissionsClick: () -> Unit = {},
-    // onPermissionsLearnMoreClick: () -> Unit = {},
-    onAcceptPermissionClick: () -> Unit = {},
-    onDismissPermissionDialog: () -> Unit = {},
-) {
-    val hasPermissions = hasOverlayDrawPermission && hasAccessibilityPermission
-
-    Column {
-        if (!hasPermissions) {
-            PermissionsBanner(
-                hasAccessibilityPermission = hasAccessibilityPermission,
-                hasOverlayDrawPermission = hasOverlayDrawPermission,
-                onFixClick = onFixPermissionsClick,
-                // onLearnMoreClick = onPermissionsLearnMoreClick,
-            )
-        }
-        Column(
-            modifier = modifier.padding(16.dp),
-        ) {
-            ServerConfigForm(
-                modifier = Modifier.weight(1f),
-                serverConfig = serverConfig,
-                onChange = onServerConfigChange,
-            )
-            Button(
-                modifier = Modifier.fillMaxWidth(),
-                enabled = if (barrierClientConnected) true else hasPermissions,
-                onClick = onConnectClick,
-            ) {
-                Text(
-                    style = MaterialTheme.typography.button,
-                    text = stringResource(
-                        id = if (barrierClientConnected) R.string.disconnect else R.string.connect
-                    ).uppercase()
-                )
-            }
-        }
-    }
-
-    if (showOverlayDrawPermissionDialog || showAccessibilityPermissionDialog) {
-        AlertDialog(
-            text = {
-                Text(
-                    text = stringResource(
-                        when {
-                            showOverlayDrawPermissionDialog -> R.string.requires_overlay_perm_detail
-                            else -> R.string.requires_accessibility_perm_detail
-                        }
-                    )
-                )
-            },
-            confirmButton = {
-                TextButton(onClick = onAcceptPermissionClick) {
-                    Text(
-                        text = stringResource(R.string.ok).uppercase(),
-                        style = MaterialTheme.typography.button,
-                    )
-                }
-            },
-            dismissButton = {
-                TextButton(onClick = onDismissPermissionDialog) {
-                    Text(
-                        text = stringResource(R.string.cancel).uppercase(),
-                        style = MaterialTheme.typography.button,
-                    )
-                }
-            },
-            onDismissRequest = onDismissPermissionDialog,
-        )
-    }
-}
-
-@Composable
-private fun PermissionsBanner(
-    modifier: Modifier = Modifier,
-    hasAccessibilityPermission: Boolean,
-    hasOverlayDrawPermission: Boolean,
-    onFixClick: () -> Unit,
-    // onLearnMoreClick: () -> Unit,
-) {
-    FixPermissionsBanner(
-        modifier = modifier,
-        text = {
-            Text(
-                text = stringResource(
-                    when {
-                        !hasAccessibilityPermission && !hasOverlayDrawPermission -> R.string.requires_accessibility_overlay_perms
-                        !hasAccessibilityPermission -> R.string.requires_accessibility_perm
-                        else -> R.string.requires_overlay_perm
-                    }
-                )
-            )
-        },
-        onFixClick = onFixClick,
-        // onLearnMoreClick = onLearnMoreClick,
-    )
-    Divider()
-}
-
-@Preview
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
-@Composable
-private fun PreviewHomeScreenContent() {
-    BarrierClientTheme {
-        Surface {
-            HomeScreenContent()
-        }
-    }
 }
 
 private fun connect(
