e49c03296e98cd34abe6c93e85188a50034c4ed7
Ammar Githam <ammar.githam@outlook.com>
2022-02-19 16:12:57 +0900

    Remove old Log classes

--- app/src/main/java/org/synergy/BarrierClientApp.kt
+++ app/src/main/java/org/synergy/BarrierClientApp.kt
@@ -3,7 +3,6 @@ package org.synergy
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import org.synergy.barrier.base.utils.BarrierDebugTree
-import org.synergy.barrier.base.utils.Log
 import org.synergy.barrier.base.utils.Timber
 
 @HiltAndroidApp
@@ -12,9 +11,6 @@ class BarrierClientApp : Application() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(BarrierDebugTree())
-            Log.logLevel = Log.Level.DEBUG
-        } else {
-            Log.logLevel = Log.Level.ERROR
         }
     }
 }
